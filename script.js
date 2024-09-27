753eb865856d787fde385046f8a513d1ba864f57
milenakosh <147971990+milenakosh@users.noreply.github.com>
2024-09-27 20:24:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+let x = 5;
+x = String(x);
+console.log(x);
+
+let s = "10";
+s = Number(s);
+console.log(s);
+
+let y = 'hello';
+y = String(y);
+console.log(y);
+
+
+
+let d = "20.5";
+d = Number(d);
+console.log(d);
+
+let z = true;
+
+console.log(z)
+
+let f = "30px"
+f = String(f)
+console.log(f)
+
+let w = null;
+
+console.log(w);
+
+let v;
+
+console.log(v);
+
+let h = "10.5";
+h = Number(h);
+console.log(h);
+
+let b = "20";
+b = Number(b);
+console.log(b);
+
+let m = "30.25px";
+m = String(m);
+console.log(m);
+
+
+let userName = "Джон";
+console.log(userName);
+let age = 30;
+
+console.log(`Меня зовут${userName} мне${age} лет`)
+
+let love = ("Я изучаю JavaScript");
+love = alert(love);
+console.log(love);
+
+let num = (123);
+num = alert(num);
+console.log(num);
+
+let a = (1 + 2 + 3);
+a = Number(a);
+a = alert(a);
+console.log(a);
+
+let l = (1);
+l = prompt(2)
+console.log(34 + k);
+
+
